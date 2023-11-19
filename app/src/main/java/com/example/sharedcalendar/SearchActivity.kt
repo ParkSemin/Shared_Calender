f297ee9b08bb1d9b36ae78ebebc4dfb7dac60745
akchon2000 <pkh2956@naver.com>
2023-11-20 00:13:17 +0900

    검색 설정 추가 schedule_item.xml 추가 메인 화면에 툴바 위의 오른쪽에 검색 아이콘 추가 메인 엑티비티 onOptionsItemSelected 함수 코드 추가 activity_search 레이아웃 추가 SearchActivity.kt 추가 SearchAdapter.kt 추가

--- app/src/main/java/com/example/sharedcalendar/SearchActivity.kt
+++ app/src/main/java/com/example/sharedcalendar/SearchActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sharedcalendar.databinding.ActivitySearchBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -14,71 +15,74 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 
-class SearchActivity : AppCompatActivity() {
 
-    private lateinit var databaseReference: DatabaseReference
-    private lateinit var adapter: SearchAdapter // 사용자 정의 어댑터
-    private val searchResults = mutableListOf<ScheduleData>() // 검색 결과를 저장할 리스트
+    class SearchActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
-        val searchRecyclerView = findViewById<RecyclerView>(R.id.searchRecyclerView)
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        private lateinit var databaseReference: DatabaseReference
+        private lateinit var adapter: SearchAdapter // 사용자 정의 어댑터
+        private val searchResults = mutableListOf<ScheduleData>() // 검색 결과를 저장할 리스트
 
+        private lateinit var binding: ActivitySearchBinding // 데이터 바인딩 객체
 
-        // Firebase 데이터베이스 참조 초기화
-        databaseReference = FirebaseDatabase.getInstance().reference.child("schedules").child(MyApplication.email_revised.toString())
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivitySearchBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        // RecyclerView 설정
-        adapter = SearchAdapter(searchResults)
-        searchRecyclerView.layoutManager = LinearLayoutManager(this)
-        searchRecyclerView.adapter = adapter
+            // Firebase 데이터베이스 참조 초기화
+            databaseReference = FirebaseDatabase.getInstance().reference.child("schedules")
+                .child(MyApplication.email_revised.toString())
 
-        // EditText에 텍스트 입력 시 검색 수행
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // 이전 텍스트 변경 이벤트
-            }
+            // RecyclerView 설정
+            adapter = SearchAdapter(searchResults)
+            binding.searchRecyclerView.layoutManager = LinearLayoutManager(this)
+            binding.searchRecyclerView.adapter = adapter
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 텍스트 변경 중 이벤트
-            }
+            // EditText에 텍스트 입력 시 검색 수행
+            binding.searchEditText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // 이전 텍스트 변경 이벤트
+                }
 
-            override fun afterTextChanged(s: Editable?) {
-                // 텍스트 변경 후 이벤트
-                val query = s.toString().trim()
-                searchDatabase(query)
-            }
-        })
-    }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // 텍스트 변경 중 이벤트
+                }
 
-    private fun searchDatabase(query: String) {
-        // Firebase 데이터베이스에서 검색 쿼리 수행
-        val queryRef = databaseReference.orderByChild("title")
+                override fun afterTextChanged(s: Editable?) {
+                    // 텍스트 변경 후 이벤트
+                    val query = s.toString().trim()
+                    searchDatabase(query)
+                }
+            })
+        }
 
-        queryRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                searchResults.clear()
-                for (snapshot in dataSnapshot.children) {
-                    val scheduleData = snapshot.getValue(ScheduleData::class.java)
-                    scheduleData?.let {
-                        if (it.title.contains(query, ignoreCase = true)) {
-                            // 검색어가 일정명에 포함되어 있는 경우에만 추가
-                            searchResults.add(it)
+        private fun searchDatabase(query: String) {
+            // Firebase 데이터베이스에서 검색 쿼리 수행
+            val queryRef = databaseReference.orderByChild("title")
+
+            queryRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    searchResults.clear()
+                    for (snapshot in dataSnapshot.children) {
+                        val scheduleData = snapshot.getValue(ScheduleData::class.java)
+                        scheduleData?.let {
+                            if (it.title.contains(query, ignoreCase = true)) {
+                                // 검색어가 일정명에 포함되어 있는 경우에만 추가
+                                searchResults.add(it)
+                            }
                         }
                     }
+                    adapter.notifyDataSetChanged()
                 }
-                adapter.notifyDataSetChanged()
-            }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                // 검색 오류 처리
-            }
-        })
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // 검색 오류 처리
+                }
+            })
+        }
     }
-
-
-}
-
-
--- app/src/main/java/com/example/sharedcalendar/SearchAdapter.kt
+++ app/src/main/java/com/example/sharedcalendar/SearchAdapter.kt
@@ -1,40 +1,42 @@
 package com.example.sharedcalendar
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sharedcalendar.databinding.ScheduleItemBinding
 
 class SearchAdapter(private val scheduleList: List<ScheduleData>) : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
 
+    // 뷰 홀더를 생성하고 뷰를 연결하는 부분
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.schedule_item, parent, false)
-        return ViewHolder(view)
+        // 데이터 바인딩을 사용하여 레이아웃을 inflate하고 바인딩 객체 생성
+        val binding = ScheduleItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
+    // 뷰 홀더와 데이터를 바인딩하는 부분
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val schedule = scheduleList[position]
         holder.bind(schedule)
     }
 
+    // 어댑터가 관리하는 아이템의 개수 반환
     override fun getItemCount(): Int {
         return scheduleList.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        val startDateTextView: TextView = itemView.findViewById(R.id.startDateTextView)
-        val startTimeTextView: TextView = itemView.findViewById(R.id.startTimeTextView)
-        val endDateTextView: TextView = itemView.findViewById(R.id.endDateTextView)
-        val endTimeTextView: TextView = itemView.findViewById(R.id.endTimeTextView)
+    inner class ViewHolder(private val binding: ScheduleItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        // ViewHolder 내의 바인딩된 View들을 참조하는 변수들
+        // 예: binding.titleTextView, binding.startDateTextView, ...
 
         fun bind(schedule: ScheduleData) {
-            titleTextView.text = schedule.title
-            startDateTextView.text = schedule.start_date
-            startTimeTextView.text = schedule.start_time
-            endDateTextView.text = schedule.end_date
-            endTimeTextView.text = schedule.end_time
+            // ViewHolder 내의 View에 데이터를 바인딩하는 함수
+            binding.titleTextView.text = schedule.title // 일정 제목 텍스트 설정
+            binding.startDateTextView.text = schedule.start_date // 시작 날짜 텍스트 설정
+            binding.startTimeTextView.text = schedule.start_time // 시작 시간 텍스트 설정
+            binding.endDateTextView.text = schedule.end_date // 종료 날짜 텍스트 설정
+            binding.endTimeTextView.text = schedule.end_time // 종료 시간 텍스트 설정
             // 다른 필드에 대한 바인딩도 추가 가능
         }
     }
